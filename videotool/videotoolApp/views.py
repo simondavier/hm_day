117cb176b4b1dd6df7b95aa0d87a7a067dfb116c
simondavier <simondavier@sina.com.cn>
2018-11-01 10:55:53 +0800

    modify views

--- videotool/videotoolApp/views.py
+++ videotool/videotoolApp/views.py
@@ -8,30 +8,41 @@ from libs import serial_connection as serial
 from libs import telnet_operation as telnet
 import time
 #sc = serial.SerialConnection()
-
+debugmode = 1 # if this value is 1, click connect will not use real telnet to link device and master
 def showtest(request):
     return render(request,'test.html')
 
 def connection(request):
-    qdmodel = request.POST.get("qdmodel")
-    if qdmodel == '780E':
-        sc = serial.SerialConnection()
-        comName = sc.get_serial_name()
+    if debugmode==0:
+        qdmodel = request.POST.get("qdmodel")
+        if qdmodel == '780E':
+            sc = serial.SerialConnection()
+            comName = sc.get_serial_name()
+        else:
+            comName = "None"
+        sc.serial_close()
+        return JsonResponse({"comName":comName})
     else:
-        comName = "None"
-    sc.serial_close()
-    return JsonResponse({"comName":comName})
+        return JsonResponse({"comName":'COM99'})
 
 def showdevice(request):
-    ipAddress = request.POST.get("ipaddress")
-    username = request.POST.get("username")
-    password = request.POST.get("password")
-    print(ipAddress)
-    tn = telnet.TelnetApp(ipAddress,username,password)
-    out = tn.excut_command('show device')
-    print(out)
-    device_number,device_name,ip_device,device_serial= tn.parse_get_device_and_ip(out)
-    return JsonResponse({"device_name":device_name,"device_number":device_number})
+    
+    if  debugmode==0:
+        ipAddress = request.POST.get("ipaddress")
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        print(ipAddress)
+        tn = telnet.TelnetApp(ipAddress,username,password)
+        out = tn.excut_command('show device')
+        print(out)
+        device_number,device_name,ip_device,device_serial= tn.parse_get_device_and_ip(out)
+        return JsonResponse({"device_name":device_name,"device_number":device_number})
+    else:
+        device_number = ['00000', '01301', '01401', '05001', '05008', '05009', '05555', '32001', '32002', '32003']
+        device_name = ['(00396)NX-1200 Master', '(00526)CTP-1301', '(00528)VPX-1401', '(00397)NX-1200', '(00530)PR01-0808', '(01383)PR01-RX', '(00530)PR01-0808', '(00526)CTP-1301', '(00262)NSX Application', '(00262)NSX Application']
+        
+        return JsonResponse({"device_name":device_name,"device_number":device_number})
+        
 
 import csv
 from .models import Resolution
